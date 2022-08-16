74a4434775e36bc75aa5960c772d7c7988b82749
Admin-Santa-Casa-DevWEB <devweb@santacasacg.org.br>
2022-08-16 15:57:55 -0400

    Consertando problema no seeder e comentando linhas de procedimento alternativas caso de erro e probabilidade de manutenção rápida

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -7,29 +7,24 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
-class CreateAdminUserSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
+class CreateAdminUserSeeder extends Seeder{
+
     public function run()
     {
         $user = User::create([
-            'name' => 'Sandonas', 
             'email' => 'sandonas@gmail.com',
             'numberCard' =>'00777',
             'dateBorn' =>'15/04/1998',
             'password' => bcrypt('12345')
         ]);
-    
+
         $role = Role::create(['name' => 'Admin']);
-     
+
         $permissions = Permission::pluck('id','id')->all();
-   
+
         $role->syncPermissions($permissions);
-     
+        // print_r($user);
         $user->assignRole([$role->id]);
     }
 }
